4eec0f7141fe09fcfe9127ae36bfc3de03a42ba7
Pradeeksha006 <pradeeksha2006@gmail.com>
2026-07-04 16:16:54 +0530

    Fix import crash: add missing Link import from react-router-dom in DeptHeadDashboard.jsx

--- frontend/src/pages/DeptHeadDashboard.jsx
+++ frontend/src/pages/DeptHeadDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import api from '../services/api';
 import { 
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer 
